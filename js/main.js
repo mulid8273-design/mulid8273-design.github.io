38f88ba78dbd162d236043ac15d6e2767941c00b
mulid8273-design <mulid8273@gmail.com>
2025-12-02 13:49:13 +0900

    Add event listeners to tool buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('main loaded')
+console.log('main loaded');
+
+document.querySelectorAll('.tool-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        alert(btn.innerText + " 기능 준비중!");
+    });
+});
