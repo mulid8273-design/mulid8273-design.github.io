8950a51ed23f33ea74afed56ce69d0b87d113f20
mulid8273-design <mulid8273@gmail.com>
2025-12-06 21:08:45 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-console.log("MULID 변환 사이트 로딩 완료");
+// 공통 유틸 + 다운로드 함수
 
-// 공통 알림 기능
-function notify(msg) {
-    alert(msg);
+// 브라우저에서 파일 다운로드
+export function downloadFile(blob, filename) {
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+}
+
+// DOM helper
+export function $(id) {
+  return document.getElementById(id);
 }
